ae38ab5d64fda8b9f533d80c70e7816f9adc4432
SagarUbale <sagarubal@gtasterix.com>
2024-10-19 18:17:49 +0530

    total changes

--- ABhinavBharatAllFiles/HomePage/image/script.js
+++
@@ -1,8 +0,0 @@
-// Animation on scroll
-window.addEventListener('scroll', function() {
-    const animatedImage = document.getElementById('animatedImage');
-    const rect = animatedImage.getBoundingClientRect();
-    if (rect.top < window.innerHeight) {
-        animatedImage.classList.add('visible');
-    }
-});
